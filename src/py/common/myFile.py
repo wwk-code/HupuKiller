2e5ebea4994b26af9dfe0c474f68c942844f9761
root <root@DESKTOP-9RQB5NI.>
2024-12-10 14:57:42 +0800

    Update

---
+++ src/py/common/myFile.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import os,sys,json
+
+project_root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+py_src_root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(py_src_root_dir)
+
+
+def read_csv_file(csv_file_path: str):
+    try:
+        data = pd.read_csv(csv_file_path)
+        return data
+    except FileNotFoundError:
+        print(f"文件未找到: {csv_file_path}")
+        return None
+    except pd.errors.EmptyDataError:
+        print("文件是空的")
+        return None
+    except pd.errors.ParserError:
+        print("文件解析错误")
+
+
+def loadJsonFile(json_file_path):
+    with open(json_file_path, 'r', encoding='utf-8') as f:
+        jsonContent = json.load(f)
+    return jsonContent
+
+
+def append_to_json_file(file_path, new_data):
+    if not os.path.exists(file_path):
+        data = []
+    else:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError:
+                data = []
+    data.extend(new_data)
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+# 清空文件内容
+def refreashFile(filePath):
+    with open(filePath,mode='w') as f:
+        pass  
+
+
+
+
+
--- src/py/dataProcessing/csvProcessing.py
+++ src/py/dataProcessing/csvProcessing.py
@@ -1,19 +1,111 @@
 import pandas as pd
+import os,sys,json
+
+# 项目根目录
+project_root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+# 项目python源码目录
+py_src_root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(py_src_root_dir)
 
 
+from common.myFile import *
 
-# 指定 CSV 文件的路径
-csv_file_path = 'path/to/your/file.csv'
 
-# 读取 CSV 文件
-try:
-    data = pd.read_csv(csv_file_path)
+class NBAFinalAverageDataCsvProcessor():
 
-    # 打印读取的数据
-    print(data)
-except FileNotFoundError:
-    print(f"文件未找到: {csv_file_path}")
-except pd.errors.EmptyDataError:
-    print("文件是空的")
-except pd.errors.ParserError:
-    print("文件解析错误")
+    def __init__(self,numOfYears=3):
+        self.numOfYears = numOfYears
+        pass
+
+    def extractNBAFinalAverageData(self):
+        # 获取当前csv表中每个年份主客队球员场均数据子表的起始行索引
+        def getEveryYearDataStartIndex(df: pd.DataFrame):
+            startIndex = []
+            numOfRows = len(df)
+            lastIndex = 0
+            for i in range(1,numOfRows):
+                if df.loc[i][0][:4].isdigit():
+                    startIndex.append([lastIndex,i])
+                    lastIndex = i
+            startIndex.append([lastIndex,numOfRows])
+            return startIndex
+        
+        averageFinalGameDatas = []
+        NBAFinalAverageDataCsvPath = os.path.join(project_root_dir,"outputs","NBAFinalsStatisticCrawler","finalAverageDatas.csv")
+        df = read_csv_file(NBAFinalAverageDataCsvPath)
+        startIndex = getEveryYearDataStartIndex(df)
+
+        for i in range(0,2 * self.numOfYears,2):
+            winnerDatasDict = {}
+            winnerDatasDict['title'] = df.loc[startIndex[i][0]][0]  # 标题  （字符串）
+            winnerDatasDict['headers'] = df.loc[startIndex[i][0]+1]  # 表头  (一级列表)
+            winnerDatasDict['items'] = df.loc[startIndex[i][0]+2:startIndex[i][1] - 1].values.tolist()  # 表项 (二级列表)
+            winnerDatasDict['year'] = winnerDatasDict['title'][:4]  
+
+            loserDatasDict = {}
+            loserDatasDict['title'] = df.loc[startIndex[i+1][0]][0]  # 标题  （字符串）
+            loserDatasDict['headers'] = df.loc[startIndex[i+1][0]+1]  # 表头  (一级列表)
+            loserDatasDict['items'] = df.loc[startIndex[i+1][0]+2:startIndex[i+1][1] - 1].values.tolist()  # 表项 (二级列表)
+            loserDatasDict['year'] = loserDatasDict['title'][:4]
+
+            averageFinalGameDatas.append({"winner" : winnerDatasDict,"loser" : loserDatasDict})
+            
+        return averageFinalGameDatas
+        
+        
+
+    def exportAverageFinalGameDatas2SftJson(self,averageFinalGameDatas: list):
+        
+        # 输出SFTQA对文件路径
+        outputSFTQAFilePath = os.path.join(project_root_dir,"outputs","SFTDatas","NBAFinalAverageDatasQA.json")
+
+        # 根据 assets/sft/SFTQATemplate.json 中的  nbaFinalAverageData_QA_template_1 构造对应SFT QA对
+        def constructNbaFinalAverageDataQATemplate_1(datas:dict):
+            SFTQATemplateFilePath = os.path.join(project_root_dir,"assets","sft","SFTQATemplate.json")
+            jsonContent = loadJsonFile(SFTQATemplateFilePath)
+            year = datas['year']
+            headers = datas['headers']
+            items = datas['items']
+            nbaFinalAverageDataQATemplates = []
+            rwaNbaFinalAverageDataQATemplate = jsonContent['nbaFinalAverageData_QA_template_1']
+            for item in items:
+                nbaFinalAverageDataQATemplate = rwaNbaFinalAverageDataQATemplate.copy()
+                datasTemplate = ""
+                for i in range(1,len(item)):
+                    seperator = ' , ' if i != len(item) - 1 else '.'
+                    datasTemplate = datasTemplate + headers[i] + " : " + item[i] + seperator
+                fillData = {
+                    "player" : item[0],
+                    "year" : year,
+                    "datas" : datasTemplate
+                }
+                nbaFinalAverageDataQATemplate['input'] = nbaFinalAverageDataQATemplate['input'].format(**fillData)
+                nbaFinalAverageDataQATemplate['output'] = nbaFinalAverageDataQATemplate['output'].format(**fillData)
+                nbaFinalAverageDataQATemplates.append(nbaFinalAverageDataQATemplate)
+
+            return nbaFinalAverageDataQATemplates
+
+        refreashFile(outputSFTQAFilePath)
+
+        for averageFinalGameData in averageFinalGameDatas:
+            winnerData = averageFinalGameData['winner']
+            loserData = averageFinalGameData['loser']
+
+            nbaFinalWinnerAverageDataQATemplates = constructNbaFinalAverageDataQATemplate_1(winnerData)
+            nbaFinalLoserAverageDataQATemplates = constructNbaFinalAverageDataQATemplate_1(loserData)
+
+            append_to_json_file(outputSFTQAFilePath,nbaFinalWinnerAverageDataQATemplates)
+            append_to_json_file(outputSFTQAFilePath,nbaFinalLoserAverageDataQATemplates)
+
+    def extractNBAFinalAverageDatasCsvAndExportQA(self):
+        averageFinalGameDatas = self.extractNBAFinalAverageData()
+        self.exportAverageFinalGameDatas2SftJson(averageFinalGameDatas)
+
+
+if __name__ == "__main__":
+
+    numOfYears = 4
+
+    # Extract NBAFinalAverageData and export to QA json file
+    nbaFinalAverageDataCsvProcessor = NBAFinalAverageDataCsvProcessor(numOfYears = numOfYears)
+    nbaFinalAverageDataCsvProcessor.extractNBAFinalAverageDatasCsvAndExportQA()
